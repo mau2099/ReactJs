65f5bb5c88cab75ffbbb6de36d850ea038f1b070
Mauricio Garcia <mau200729@gmail.com>
2019-09-15 07:04:08 -0500

    cambios para el proyecto de react con oscar barajas

---
+++ holamundo2099/src/components/HolaMundo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const  HolaMundo = ()=> {
+    return{
+        // <div>
+        // </div>
+    };
+};
+
+export default HolaMundo; 
---
+++ holamundo2099/src/components/Presentational.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Presentational = ()=> <h1>Hola mundo</h1>;
+
+export default Presentational;
