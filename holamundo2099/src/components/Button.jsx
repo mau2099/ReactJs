c66be37c1d3c03edeaf3692b4bcb311a0ee9fba0
Mauricio Garcia <mau200729@gmail.com>
2019-10-01 15:08:47 -0500

    se hace ejemplo de eventos en react

--- holamundo2099/src/components/Button.jsx
+++ holamundo2099/src/components/Button.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 
-const Button = props => {
-    const { text, text2 } = props;
-  return (
-    <div>
-      <button type="button">{props.text}</button>
-      <button type="button">{text2}</button>
-    </div>
-  );
-};
+//Método con class
+class Button extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleClick = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  render() {
+    //Destructuracion
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Manzanas: {count} </h1>
+        <button type="button" onClick={this.handleClick}>Click</button>
+      </div>
+    );
+  }
+}
+
+//Método Button en Stateless
+// const Button = props => {
+//   const { text, text2 } = props;
+//   return (
+//     <div>
+//       <button type="button">{props.text}</button>
+//       <button type="button">{text2}</button>
+//     </div>
+//   );
+// };
 
 export default Button;
